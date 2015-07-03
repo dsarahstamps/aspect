415167bf18a6293536ed25fd4795b6c10a64c08d
D. Sarah Stamps <stamps.dsarah@gmail.com>
2015-07-03 12:46:11 -0500

    remove incorrect g5_CEAR that was run at g4 and updated run_traction.bash script

--- run_traction.bash
+++ run_traction.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -J run_g5_CEAR_vtu
-#SBATCH -o g5_CEAR.o%j
+#SBATCH -J run_g6_CEAR_vtu
+#SBATCH -o g6_CEAR.o%j
 #SBATCH -n 64 
 #SBATCH -p normal 
-#SBATCH -t 08:00:00
-ibrun $WORK/aspect/build/aspect g5_CEAR.prm
+#SBATCH -t 28:00:00
+ibrun $WORK/aspect/build/aspect g6_CEAR.prm
