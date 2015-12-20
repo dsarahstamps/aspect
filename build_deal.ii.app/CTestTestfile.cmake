48f96a8ec90363c83692ae51fb54ff030dd999b3
dsarahstamps <stamps.dsarah@gmail.com>
2015-12-20 18:19:31 -0500

    remove deal.ii app from current repository

--- build_deal.ii.app/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/q/Documents/aspect
-# Build directory: /Users/q/Documents/aspect/build_deal.ii.app
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(tests)
