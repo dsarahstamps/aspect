3f23fe1cf66751f47d2b87469384111d8e92aefa
Dorothy Stamps Glesener <dstamps@nrlogin1.cluster>
2016-12-20 09:33:28 -0500

    Updating local directory with new computations.

--- submit_aspect_job_matt.sh
+++ submit_aspect_job_matt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
-#PBS -N g3_point
-#PBS -o g3_point
-#PBS -l nodes=1:ppn=20
+#PBS -N g5_vtu
+#PBS -o g5_vtu 
+#PBS -l nodes=1:ppn=10
 #PBS -q stamps_geo_lab
 #PBS -l walltime=48:00:00 
 #PBS -W group_list=stamps_geo_lab
@@ -10,4 +10,4 @@
 module purge
 module load gcc openmpi/1.10.0 glm lua python boost hdf5 phdf5 atlas trilinos p4est dealii
 cd $WORK/aspect
-mpirun -np $PBS_NP ./build/aspect g3_point_values.prm | tee jobout_g3_point.$PBS_JOBID
+mpirun -np $PBS_NP ./build/aspect g5_traction_vector_rerun_v4.prm &> jobout_g5_vtu.$PBS_JOBID
