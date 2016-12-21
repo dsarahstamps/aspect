913d47bf6bb56184e59684cc21f5d95c0feb0081
Dorothy Stamps Glesener <dstamps@nrlogin1.cluster>
2016-12-21 11:18:01 -0500

    Tests- convergence issues, now updating to latest version of ASPECT

--- submit_aspect_job.sh
+++ submit_aspect_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
-#PBS -N g3_point
-#PBS -o g3_point
+#PBS -N g3_point_t1
+#PBS -o g3_point_t1
 #PBS -l nodes=1:ppn=20
 #PBS -q stamps_geo_lab
-#PBS -l walltime=48:00:00 
+#PBS -l walltime=4:00:00 
 #PBS -W group_list=stamps_geo_lab
 #PBS -A mantLith
 
 module purge
 module load gcc openmpi/1.10.0 glm lua python boost hdf5 phdf5 atlas trilinos p4est dealii
 cd $WORK/aspect
-mpirun -np $PBS_NP ./build/aspect g3_point_values.prm | tee jobout_g3_point.$PBS_JOBID
+mpirun -np $PBS_NP ./build/aspect g3_traction_vector_t1.prm | tee jobout_g3_point_t1.$PBS_JOBID
